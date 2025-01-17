6f121d57a6c5a508aa9c411fff52141930765d75
Bumpy_Lumps <alex@unlimitedtechsolutions.com>
2025-01-17 16:09:26 -0500

    first commit

---
+++ client/src/components/Posts.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import { useApi } from '../contexts/ApiProvider';
+import Post from  './Post';
+import More from './More';
+
+
+export default function Posts({ content }) {
+    const [posts, setPosts] = useState(); //sets state for posts
+    const [pagination, setPagination] = useState();
+    const api = useApi();
+
+    let url;
+    switch(content) {
+      case 'feed':
+      case undefined:
+        url = '/feed';
+        break;
+      case 'explore':
+        url= '/posts';
+        break
+        default:
+          url = `/users/${content}/posts`
+          break;
+    }
+
+   //side effect to request posts here
+  useEffect(() => {
+    (async () => { // use IIFE to get around react async quirk
+      const response = await api.get(url);
+      if (response.ok){
+        setPosts(response.body.data);
+        setPagination(response.body.pagination);
+      } 
+      else {
+        setPosts(null);
+      }
+    })();
+  }, [api, url]);
+
+
+  const loadNextPage = async () => {
+    const response = await api.get(url, {
+      after: posts[posts.length - 1].timestamp
+    });
+    if(response.ok) {
+      setPosts([...posts, ...response.body.data]);
+      setPagination([response.body.pagination]);
+    }
+  };
+
+  return (
+        <>
+          {posts === undefined ?
+            <Spinner animation="border" />
+          :
+            <>
+              {posts === null ?
+                <p>Could not retrieve blog posts.</p> 
+                :
+                  <>
+                    {posts.length === 0 ?
+                    <p>There are no blog posts.</p>
+                  :  
+                    posts.map(post => <Post key={post.id} post={post} />) 
+            }
+            <More pagination={pagination} loadNextPage={loadNextPage} />
+          </>
+        }
+        </>
+      }
+    </>
+  );
+}
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Body from '../components/Body';
+import InputField from '../components/InputField';
+
+
+export default function LoginPage() {
+    const [formErrors, setFormErrors] = useState({});
+    const usernameField = useRef();
+    const passwordField = useRef();
+
+    useEffect(() => {
+        usernameField.current.focus(); // DOM API focus targets field into focus for user
+    }, []);
+
+    const onSubmit = (ev) => { // ev === "event"
+        ev.preventDefault(); // disable browser's built in form submission
+        const username = usernameField.current.value;
+        const password = passwordField.current.value;
+
+        console.log(`You entered ${username}: ${password}`);
+    };
+
+
+    return (
+        <Body>
+            <h1>Login</h1>
+            <Form onSubmit={onSubmit}>
+                <InputField 
+                    name="username" label="Username or email address"
+                    error={formErrors.username} fieldRef={usernameField} />
+                <InputField
+                    name="password" label="Password" type="password"
+                    error={formErrors.password} fieldRef={passwordField} />
+                <Button variant="primary" type="submit">Login</Button>
+            </Form>
+        </Body>
+    );
+}
